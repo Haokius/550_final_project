014e8b4f8066f283cd7b68a74f75bb12f3f6896c
Kevin He <haokunkevinhe@gmail.com>
2024-11-07 16:55:46 -0500

    trying to fix scraper

--- backend/scraping/sec_sp500_scrape.py
+++ backend/scraping/sec_sp500_scrape.py
@@ -18,36 +18,20 @@ def get_sp500_ciks():
         ticker = row.find_all('td')[0].text.strip()
         name = row.find_all('td')[1].text.strip()
         cik = row.find_all('td')[6].text.strip()
-        company_ciks.append((ticker, name, str(int(cik))))
+        company_ciks.append((ticker, name, int(cik)))
     
     return company_ciks
 
 company_ciks = get_sp500_ciks()
 
-def process_feature(feature_data):
-    try:
-        processed = {
-            "label": feature_data["label"],
-            "description": feature_data["description"],
-            "values": []
-        }
-        total_rows = 0
-        for entry in feature_data["units"]["USD"]:
-            if not entry["val"] or not entry["fy"] or not entry["fp"] or not entry["form"]:
-                continue
-            processed["values"].append({
-                "val": entry["val"],
-                "fiscal_year": entry["fy"],
-                "fiscal_period": entry["fp"],
-                "form": entry["form"],
-                "frame": entry.get("frame", None)
-            })
-            total_rows += 1
-        return processed, total_rows
-    except Exception as e:
-        print(f"Error processing feature: {feature_data["label"]}")
-        print(e)
-        return None
+def process(response):
+    processed_chunk = {"ccp": response["ccp"], "feature": response["tag"], "data": []}
+    data = response["data"]
+    for company in data:
+        print(company)
+        if str(company["cik"]) in company_ciks:
+            processed_chunk["data"].append(company)
+    return processed_chunk
 
 email = "haokunkevinhe@gmail.com" # NOTE: SET YOUR OWN EMAIL HERE
 
@@ -56,74 +40,14 @@ def get_company_frames():
     edgar = EdgarClient(user_agent=f"DummyCompany {email}")
     for year in tqdm(range(2010, 2021)):
         for quarter in tqdm(range(1, 5)):
-            time.sleep(5)
             response = edgar.get_frames(year=year, quarter=quarter, taxonomy="us-gaap", tag="AccountsPayableCurrent", unit="USD")
-            output_data.append(response)
+            processed_response = process(response)
+            output_data.append(processed_response)
     
     with open("frames_data.json", "w") as f:
         json.dump(output_data, f, indent=4)
 
 get_company_frames()
-
-def get_company_facts():
-    output_data = [{"total_rows_of_everything": 0}]
-    incomplete_companies = []
-    edgar = EdgarClient(user_agent=f"DummyCompany {email}")
-
-    frequency = defaultdict(int)
-    for ticker, name, cik in company_ciks[:30]:
-        time.sleep(5)
-        company_data = {}
-        response = edgar.get_company_facts(cik=cik)
-        cik = response["cik"]
-        entity_name = response["entityName"]
-        
-        company_data = {"cik": cik, "entity_name": entity_name, "ticker": ticker, "name": name, "total_rows": 0}
-        
-        facts = response["facts"]
-        us_gaap = facts["us-gaap"]
-        
-        for feature in us_gaap.keys():
-            frequency[feature] += 1
-        
-        features_to_names = {
-            "RevenueFromContractWithCustomerExcludingAssessedTax": "revenue",
-            "NetIncomeLoss": "net_income",
-            "Assets": "assets",
-            "Liabilities": "liabilities",
-            "OperatingIncomeLoss": "operating_income",
-            "CashAndCashEquivalentsAtCarryingValue": "cash_and_equivalents",
-            "AccountsReceivableNetCurrent": "accounts_receivable",
-            "InventoryNet": "inventory",
-            "LongTermDebt": "long_term_debt",
-            "ComprehensiveIncomeNetOfTax": "comprehensive_income"
-        }
-        
-        total_rows = 0
-        for feature, name in features_to_names.items():
-            feature_data = us_gaap.get(feature, None)
-            if feature_data:
-                company_data[name], rows = process_feature(feature_data)
-                total_rows += rows
-            else:
-                incomplete_companies.append({"ticker": ticker, "feature": feature, "cik": cik})
-        company_data["total_rows"] = total_rows
-        output_data[0]["total_rows_of_everything"] += total_rows
-        output_data.append(company_data)
-
-    new_frequency = {}
-    for frequency, count in frequency.items():
-        if count == 20:
-            new_frequency[frequency] = count
-
-    with open("frequency.json", "w") as f:
-        json.dump(new_frequency, f, indent=4)
-
-    with open("incomplete_companies.json", "w") as f:
-        json.dump(incomplete_companies, f, indent=4)
-
-    with open("sp500_data.json", "w") as f:
-        json.dump(output_data, f, indent=4)
 
 # Top 10 important features:
 # 1. RevenueFromContractWithCustomerExcludingAssessedTax - Represents the company's revenue.
